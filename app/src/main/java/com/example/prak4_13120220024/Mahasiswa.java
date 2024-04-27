328d8121e45d30a38d590fc7b1295e9af09de1cd
muhfajrisbir <mhmmadfajri@gmail.com>
2024-04-28 07:12:50 +0800

    Upload Evaluasi

---
+++ app/src/main/java/com/example/prak4_13120220024/Mahasiswa.java
@@ -0,0 +1,27 @@
+package com.example.prak4_13120220018;
+
+public class Mahasiswa {
+    private String stb;
+    private String nama;
+    private int angkatan;
+
+    public Mahasiswa(String stb, String nama, int angkatan){
+        this.stb = stb;
+        this.nama = nama;
+        this.angkatan = angkatan;
+    }
+
+    public String getStb() {
+        return stb;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getAngkatan() {
+        return angkatan;
+    }
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prak4-13120220018"
+include(":app")
+ 
